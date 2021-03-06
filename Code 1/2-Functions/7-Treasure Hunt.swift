23a329a4f90baadb71051f54155fe43489e7293d
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-03-06 11:05:47 -0500

    Update 7-Treasure Hunt.swift
    i did not finish yet continue here

--- Code 1/2-Functions/7-Treasure Hunt.swift
+++ Code 1/2-Functions/7-Treasure Hunt.swift
@@ -1,1 +1,21 @@
-//
+
+func oneRowToTheLeft() {
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+}
+func oneRowForward() {
+    turnLeft()
+    moveForward()
+    moveForward()
+    toggleSwitch()
+}
+oneRowToTheLeft()
