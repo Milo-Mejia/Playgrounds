1c5ad9dae2f7d93e0d1b810eb2b9ef8124f9c318
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-03-13 11:04:45 -0500

    Update 3-To the Edge and Back.swift

--- Code 1/3-For Loops/3-To the Edge and Back.swift
+++ Code 1/3-For Loops/3-To the Edge and Back.swift
@@ -1,1 +1,18 @@
 //
+func completeRow() {
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+}
+
+for i in 1 ... 2 {
+    completeRow()
+}
+turnLeft()
+for i in 1 ... 2 {
+    completeRow()
+}
