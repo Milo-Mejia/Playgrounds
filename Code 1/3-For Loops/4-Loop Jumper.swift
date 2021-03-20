37f763ba00ce65da112c74ff7ad7fcc6834083d7
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-03-20 11:01:32 -0400

    completed
    completed

--- Code 1/3-For Loops/4-Loop Jumper.swift
+++ Code 1/3-For Loops/4-Loop Jumper.swift
@@ -1,1 +1,38 @@
 //
+func completeTheGrassyIsland() {
+    getThreeGems()
+    turnRight()
+    getThreeGems()
+    turnRight()
+    turnRight()
+    moveForward()
+    getThreeGems()
+    collectGem()
+    turnRight()
+    moveForward()
+}
+for i in 1 ... 1 {
+    completeTheGrassyIsland()
+}
+func completeSandyIsland() {
+    moveForward()
+    turnRight()
+    moveForward()
+    collectGem()
+    turnLeft()
+    moveForward()
+    moveForward()
+    turnRight()
+    moveForward()
+    collectGem()
+}
+for i in 1 ... 1 {
+    completeSandyIsland()
+}
+func getThreeGems() {
+    moveForward()
+    turnLeft()
+    moveForward()
+    moveForward()
+    collectGem()
+}
