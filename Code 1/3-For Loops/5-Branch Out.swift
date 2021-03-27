82d7d7eb986b0ae52107b61a35a24e2b107818f3
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-03-27 11:27:31 -0400

    complete

--- Code 1/3-For Loops/5-Branch Out.swift
+++ Code 1/3-For Loops/5-Branch Out.swift
@@ -1,1 +1,30 @@
 //
+func moveTowardsARow() {
+    moveForward()
+    moveForward()
+    turnRight()
+}
+
+func moveForwardSevenTimes() {
+    for i in 1 ... 7 {
+        moveForward()
+    }
+}
+
+func turnAround(){
+    turnLeft()
+    turnLeft()
+}
+func goInAndOutARow() {
+    moveForwardSevenTimes()
+    toggleSwitch()
+    turnAround()
+    moveForwardSevenTimes()
+}
+//repeat this 2 more times
+
+for i in 1 ... 3 {
+    moveTowardsARow()
+    goInAndOutARow()
+    turnRight()
+}
