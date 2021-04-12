e0166e573c652b580a647ec2a8d6c03323304a45
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-04-12 18:12:37 -0400

    Update 6-Gem Farm.swift
    incomplete

--- Code 1/3-For Loops/6-Gem Farm.swift
+++ Code 1/3-For Loops/6-Gem Farm.swift
@@ -1,1 +1,33 @@
 //
+func completeARow() {
+    toggleTheSwitches()
+    getTheGems()
+}
+func toggleTheSwitches() {
+    moveForward()
+    toggleSwitch()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+}
+func getTheGems() {
+    moveForward()
+    collectGem()
+    moveForward()
+    collectGem()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+}
+completeARow()
+for i in 1 ... 3{
+    turnLeft()
+    completeARow()
+    turnRight()
+    moveForward()
+    turnLeft()
+}
