9dcb97a6a5799ecf669bf5059b29cd9dd58133cc
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-04-17 10:57:56 -0400

    Update 7-Treasure Hunt.swift

--- Code 1/2-Functions/7-Treasure Hunt.swift
+++ Code 1/2-Functions/7-Treasure Hunt.swift
@@ -1,21 +1,42 @@
 
+func goBackToTheCenter() {
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+}
+
+func toggleAllSwitches(){
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    moveForward()
+    moveForward()
+    toggleSwitch()
+}
+
 func oneRowToTheLeft() {
-    moveForward()
-    moveForward()
-    toggleSwitch()
-    moveForward()
-    toggleSwitch()
-    turnLeft()
-    turnLeft()
-    moveForward()
-    moveForward()
-    moveForward()
-    moveForward()
+    toggleAllSwitches()
+    goBackToTheCenter()
 }
 func oneRowForward() {
     turnLeft()
     moveForward()
     moveForward()
     toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
 }
-oneRowToTheLeft()
+func completeAllRows() {
+    turnLeft()
+    oneRowToTheLeft()
+    oneRowToTheLeft()
+    oneRowForward()
+    oneRowForward()
+}
+
+completeAllRows()
