09f5e6cf188bb6db4f1e476f62985aef133e57d3
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-04-24 11:07:54 -0400

    Update 7-Four Stash Sweep.swift

--- Code 1/3-For Loops/7-Four Stash Sweep.swift
+++ Code 1/3-For Loops/7-Four Stash Sweep.swift
@@ -1,1 +1,29 @@
 //
+//pattern is three; gems; and; then; one; at; the; top
+    //
+
+func pickUpThreeGems() {
+    collectGem()
+    moveForward()
+    collectGem()
+    moveForward()
+    collectGem()
+}
+func pickUpRemaing1() {
+    moveForward()
+    collectGem()
+}
+turnLeft()
+moveForward()
+turnRight()
+moveForward()
+turnRight()
+
+
+for i in 1 ... 15{
+    pickUpThreeGems()
+    moveForward()
+    pickUpRemaing1()
+    turnLeft()
+    
+}
