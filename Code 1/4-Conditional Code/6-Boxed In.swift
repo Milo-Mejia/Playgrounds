4cc6583b1573c40d24dce7129939d9a44dc5b548
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-05-22 11:19:10 -0400

    Update 6-Boxed In.swift

--- Code 1/4-Conditional Code/6-Boxed In.swift
+++ Code 1/4-Conditional Code/6-Boxed In.swift
@@ -1,1 +1,19 @@
 //
+moveForward()
+turnLeft()
+for i in 1 ... 8{
+    makeDecisionOnHowToCompleteBox()
+    moveForward()
+    }
+    
+func makeDecisionOnHowToCompleteBox() {
+    if isOnClosedSwitch {
+        toggleSwitch()
+    }
+    if isOnGem {
+        collectGem()
+    }
+    if isBlocked {
+        turnLeft()
+    }
+}
