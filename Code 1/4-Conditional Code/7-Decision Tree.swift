8712f59b4afbf52994d8fdbb62aacf0e11651a00
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-06-05 11:29:27 -0400

    Update 7-Decision Tree.swift

--- Code 1/4-Conditional Code/7-Decision Tree.swift
+++ Code 1/4-Conditional Code/7-Decision Tree.swift
@@ -1,1 +1,66 @@
 //
+func doRightSide() {
+    moveForward()
+    collectGem()
+    turnRight()
+    for i in 1 ... 3 {
+        moveForward()
+    }
+    turnLeft()
+    moveForward()
+    collectGem()
+    turnaround()
+    moveForward()
+    turnRight()
+    for i in 1 ... 3 {
+        moveForward()
+    }
+    turnRight()
+}
+func turnaround() {
+    turnLeft()
+    turnLeft()
+}
+
+    
+func doTheMiddle() {
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    moveForward()
+    collectGem()
+    turnaround()
+    moveForward()
+    turnLeft()
+    moveForward()
+}
+if isOnClosedSwitch {
+    moveForward()
+}
+
+if isOnGem{
+    doRightSide()
+}
+
+doTheMiddle()
+doRightSide()
+moveForward()
+toggleSwitch()
+turnLeft()
+moveForward()
+collectGem()
+
+
+ 
+
+
+    /*
+ gothrough the middle row{
+ if im on a gem then{
+ solvetheRightSide
+ }
+ if im on a closed switch{
+ sovetheLeftSide
+ }
+ }
+ */
