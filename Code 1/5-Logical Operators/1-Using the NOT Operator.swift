2f3e74dbdbffc4238817a35187de445f622f5acf
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-06-12 11:26:43 -0400

    Update 1-Using the NOT Operator.swift

--- Code 1/5-Logical Operators/1-Using the NOT Operator.swift
+++ Code 1/5-Logical Operators/1-Using the NOT Operator.swift
@@ -1,1 +1,59 @@
 //
+/*
+ if there is no gem on a tile{
+ turnleft
+ moveForward
+ moveForward
+ collecGem
+ GobackUp
+ turnLeft()
+ }else{
+ }
+ */
+
+func solveLeftSide(){
+    turnLeft()
+    moveForward()
+    moveForward()
+    collectGem()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    turnLeft()
+    moveForward()
+}
+
+
+moveForward()
+for i in 1 ... 4 {
+if isOnGem {
+    collectGem()
+    moveForward()
+}else{
+    solveLeftSide()
+}
+    
+}
+
+
+
+
+
+
+
+    /*
+for i in 1 ... 4 {
+    moveForward()
+    
+    if isOnGem {
+        collectGem()
+    }
+    if !isOnGem{
+        turnRight()
+        moveForward()
+        moveForward()
+        collectGem()
+    }
+}
+ */
