33374a0d45a3fb1a16466b2953cbefff9a917d2c
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-06-26 11:05:21 -0400

    Update 4-Checking This OR That.swift

--- Code 1/5-Logical Operators/4-Checking This OR That.swift
+++ Code 1/5-Logical Operators/4-Checking This OR That.swift
@@ -1,1 +1,27 @@
 //
+for i in 1 ... 3 {
+    moveForward()
+    collectGem()
+    }
+
+for j in 1 ... 4 {
+    moveForward()
+    collectGem()
+    doThePathToTheRight()
+}
+
+
+
+func doThePathToTheRight() {
+    if isBlockedLeft {
+        turnRight()
+        moveForward()
+        moveForward()
+        toggleSwitch()
+        turnLeft()
+        turnLeft()
+        moveForward()
+        moveForward()
+        turnRight()
+    }
+}
