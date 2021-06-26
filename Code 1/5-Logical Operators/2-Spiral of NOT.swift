1d64f12ec3767058817f52c37acf73ae9400c0c5
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-06-26 11:04:28 -0400

    Update 2-Spiral of NOT.swift

--- Code 1/5-Logical Operators/2-Spiral of NOT.swift
+++ Code 1/5-Logical Operators/2-Spiral of NOT.swift
@@ -1,1 +1,17 @@
 //
+func completeTheSpiral() {
+    
+    for i in 1 ... 13 {
+        if isBlocked{
+            turnLeft()
+        }
+        moveForward()
+    }
+}
+
+completeTheSpiral()
+toggleSwitch()
+
+
+
+
