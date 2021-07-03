e747aeed970c0f3a57c342b888aaa484dd1251f4
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-07-03 11:28:19 -0400

    Update 5-Logical Labyrinth.swift

--- Code 1/5-Logical Operators/5-Logical Labyrinth.swift
+++ Code 1/5-Logical Operators/5-Logical Labyrinth.swift
@@ -1,1 +1,33 @@
 //
+for i in 1 ... 8 {
+    moveForward()
+    allOfTheIfStatements()
+    
+}
+func goDownAndGetGem(){
+    collectGem()
+    toggleSwitch()
+    turnRight()
+    moveForward()
+    moveForward()
+    collectGem()
+    turnRight()
+    turnRight()
+    moveForward()
+    moveForward()
+    turnRight()
+}
+
+
+func allOfTheIfStatements() {
+    if isOnGem && isOnClosedSwitch {
+        goDownAndGetGem()
+    }
+    if isOnClosedSwitch {
+        toggleSwitch()
+        turnLeft()
+    }
+    if isOnGem {
+        collectGem()
+    }
+}
