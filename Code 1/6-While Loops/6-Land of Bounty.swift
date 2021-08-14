85a4f3e9f5e6dbb85670bb10c3e99a2c8d8facef
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-08-14 11:02:27 -0400

    Update 6-Land of Bounty.swift

--- Code 1/6-While Loops/6-Land of Bounty.swift
+++ Code 1/6-While Loops/6-Land of Bounty.swift
@@ -1,1 +1,72 @@
 //
+    
+    
+    /*
+ func solveRowWithSwitches(){
+ wile im not blocked move forward and toggle any closed switches
+ }
+ solveRowWithSwitches()
+ soveRowWithGems()
+ solveRowWithSwitches()
+ 
+moveForward()
+
+ while isOnClosedSwitch || isOnGem{
+toggleSwitch()
+
+moveForward()
+
+collectGem()
+
+moveForward()
+
+ }
+ */
+moveForward()
+
+func solveOneRow(){
+    for i in 1 ... 5 {
+        func toglleAllSwithes() {
+            
+            if isOnClosedSwitch {
+                toggleSwitch()
+                moveForward()
+            }
+            if isOnOpenSwitch {
+                moveForward()
+            }
+            
+        }
+        while isBlocked {
+            turnRight()
+            moveForward()
+            turnRight()
+            
+        }
+        
+        toglleAllSwithes()
+        
+    }
+}
+
+
+
+func getAllGems() {
+    while isOnGem {
+        collectGem()
+        moveForward()
+    }
+    
+    while isBlocked {
+        turnLeft()
+        moveForward()
+        turnLeft()
+    }
+}
+
+
+solveOneRow()
+moveForward()
+getAllGems()
+moveForward()
+solveOneRow()
