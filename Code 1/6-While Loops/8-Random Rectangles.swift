e966e6e5b520568192d9d68d2884bc5322b70515
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-08-21 11:32:29 -0400

    Update 8-Random Rectangles.swift

--- Code 1/6-While Loops/8-Random Rectangles.swift
+++ Code 1/6-While Loops/8-Random Rectangles.swift
@@ -1,1 +1,15 @@
 //
+for i in 1 ... 4{
+    while !isBlocked {
+        moveForward()
+    }
+    while isBlocked {
+        turnRight()
+    }
+    if isOnClosedSwitch {
+        toggleSwitch()
+        
+    }
+    }
+
+
