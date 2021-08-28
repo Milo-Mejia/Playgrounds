be4483518c04266cf0795ecef9d45ebfcf8a1605
Milo-Mejia <79746803+Milo-Mejia@users.noreply.github.com>
2021-08-28 11:21:01 -0400

    Update 9-You r Always Right.swift

--- Code 1/6-While Loops/9-You r Always Right.swift
+++ Code 1/6-While Loops/9-You r Always Right.swift
@@ -1,1 +1,20 @@
 //
+moveForward()
+moveForward()
+func completteRow() {
+    while !isBlocked {
+        
+        if isOnClosedSwitch {
+            toggleSwitch()
+        }
+        else{
+            moveForward()
+        }
+        
+    }
+}
+while !isOnGem {
+    completteRow()
+    turnRight()
+}
+collectGem()
